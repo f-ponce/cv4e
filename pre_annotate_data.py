52d48476908c92d9bee9e7741d980a05a6c722fb
fponce <fponce@caltech.edu>
2022-06-19 19:04:55 -0700

    added sample json file

--- pre_annotate_data.py
+++ pre_annotate_data.py
@@ -8,7 +8,7 @@ import cv2
 import json
 ###############################################################################
 
-Datadir = '/Volumes/COMPA/upward_facing_cameras_data/pre_processed_data/pre_processed_data_0629/frames_ufc_20_01_vi_0001_20190629_171132/'
+Datadir = '/Volumes/COMPA/upward_facing_cameras_data/pre_processed_data/pre_processed_data_0629/frames_ufc_20_01_vi_0001_20190629_171133/'
 Outdir = '/Volumes/COMPA/upward_facing_cameras_data/pre_processed_data/pre_processed_data_0629/'
 ###############################################################################
 
@@ -36,12 +36,30 @@ def make_dict_per_frame(Datadir):
         all_frames_dict.append(frame_dict)
 
     return all_frames_dict
+
+def save_dict_to_json(my_list_of_dicts, my_json_filename):
+    with open(my_json_filename, "w") as write_file:
+        json.dump(my_list_of_dicts, write_file, indent=4)
+
+def add_dicts_to_json(new_list_of_dicts, my_json_filename):
+    f = open(my_json_filename)
+    data_list = json.load(f)
+    for i in range(len(new_list_of_dicts)):
+        data_list.append(new_list_of_dicts[i])
+    save_dict_to_json(data_list, my_json_filename)
+    f.close()
 ###############################################################################
 
 all_frames_dict = make_dict_per_frame(Datadir)
 
 #create json file and dump the dictionary there
 json_filename =  Outdir+'all_frames_dict.json'
-print(json_filename)
-with open(json_filename, "w") as write_file:
-    json.dump(all_frames_dict, write_file, indent=4)
+
+save_new = 1
+if save_new == 1:
+    save_dict_to_json(all_frames_dict, json_filename)
+else:
+    pass
+    #add_dicts_to_json(all_frames_dict, json_filename)
+
+print('list of dictionaries saved', json_filename)
